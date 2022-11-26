187d2f012ea223ebcd7ad6f4ef79828cec407e2b
Willian Igor <willianigordeveloper@gmail.com>
2022-11-25 23:11:33 -0300

    update Home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,17 +11,15 @@ export const Home = () => {
 
     return (
         <div className='flex flex-col h-screen md:flex-row'>
-            <div className='fixed top-5 right-5'>
-                <ThemeToggle />
+            <div className='py-20 flex flex-col items-center gap-6 w-2/3 text-center m-auto md:w-2/5 md:max-w-md'>
+                <img src="/home-undraw.svg" alt="Ilustração" />
+                <h1 className='flex items-center gap-3 text-4xl text-indigo-0'><ChatCircleDots /> Chat Room</h1>
+                <Link to="/login" className='bg-indigo-0 rounded w-full py-2 text-gray-white-1 font-semibold hover:bg-indigo-1'><button>Entrar</button></Link>
             </div>
-            <div className='flex-1 flex flex-col justify-center items-center gap-2'>
-                <h1 className='flex items-center gap-3 text-4xl text-nord-frost-1'>Chat Room<ChatCircleDots /></h1>
-                <p className='text-lg'>Seja bem vindo!</p>
-                <Link to="/login" className='bg-nord-aurora-gren text-nord-snowStorm-1 px-12 rounded font-semibold py-2 hover:opacity-90'><button>Entrar</button></Link>
-            </div>
-            <div className='flex-1 flex flex-col bg-nord-snowStorm-1 dark:bg-nord-polarNight-1 gap-4 p-4 md:justify-center md:p-8'>
-                <div className='flex flex-col gap-2'>
-                    <span className='text-nord-polarNight-1 dark:text-nord-snowStorm-1 font-medium'>Tecnologias:</span>
+
+            <div className='p-4 flex bg-gray-white-1 dark:bg-gray-black-1 items-center md:flex-col-reverse md:justify-center md:items-start md:w-2/5 md:p-8'>
+                <div>
+                    <span className='block font-medium pb-2 text-gray-black-1 dark:text-gray-white-1 md:pt-6'>Tecnologias:</span>
                     <ul className='flex flex-col gap-1'>
                         <li className='flex items-center gap-2'><img className='w-6' src={vite} alt="Vite" /> Vite</li>
                         <li className='flex items-center gap-2'><img className='w-6' src={react} alt="React" /> React</li>
@@ -30,11 +28,11 @@ export const Home = () => {
                         <li className='flex items-center gap-2'><img className='w-6' src={reactRouter} alt="React Router" /> React Router</li>
                     </ul>
                 </div>
-                <div>
+                <div className='text-right flex-1 md:flex-none md:text-start'>
+                    <strong className='pb-6 block font-semibold text-gray-black-1 dark:text-gray-white-1' >Desenvolvido por <a className='text-indigo-2 block underline' href="https://willianigordeveloper.vercel.app/" target="_blank">Willian Igor Developer</a></strong>
                     <span className='block text-sm'>Aplicativo de código aberto!</span>
-                    <a className='text-sm text-nord-frost-3' href="https://github.com/WillianIgorDeveloper/chat-room" target="_blank">Repositório do projeto no Github</a>
+                    <a className='text-sm text-nord-frost-3 text-indigo-2 underline' href="https://github.com/WillianIgorDeveloper/chat-room" target="_blank">Repositório no Github</a>
                 </div>
-                <strong className='text-center font-medium' >Desenvolvido por <a className='text-nord-frost-3' href="https://willianigordeveloper.vercel.app/" target="_blank">Willian Igor Developer</a></strong>
             </div>
         </div>
     )
