5ffe0092ba47cc7e983726caef82792c7813d0c0
Willian Igor <willianigordeveloper@gmail.com>
2022-11-29 01:40:17 -0300

    try to fixing login

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -94,7 +94,6 @@ export const App = () => {
    const [messages, setMessages] = useState()
 
    const getMessages = async () => {
-      setLoading(true)
       const { data: messages, error } = await supabase
          .from('messages')
          .select('*')
@@ -105,7 +104,6 @@ export const App = () => {
       }
 
       setMessages(messages)
-      setLoading(false)
    }
 
    useEffect(() => {
@@ -114,23 +112,35 @@ export const App = () => {
 
 
 
+   const handleNewPost = async (e) => {
+      e.preventDefault()
+      const newMessage = e.target.message.value
+      const { error } = await supabase
+         .from('messages')
+         .insert([
+            { posted_by: user.nick_name, content: newMessage },
+         ])
 
+      if (error) {
+         setServerError(true)
+         return
+      }
 
-   // const handleNewPost = async (e) => {
-   //   e.preventDefault()
-   //   const newMessage = e.target.message.value
-   //   e.target.message.value = ""
-   //   try {
-   //     const { error } = await supabase
-   //       .from('message')
-   //       .insert([
-   //         { user_id: session.user.id, message: newMessage, user_name: user.userName },
-   //       ])
-   //     if (error) throw error
-   //   } catch (error) {
-   //     setServerError(true)
-   //   }
-   // }
+      e.target.message.value = ""
+   }
+
+   useEffect(() => {
+
+      supabase.channel('messages')
+         .on(
+            'postgres_changes',
+            { event: 'INSERT', schema: 'public', table: 'messages' },
+            () => {
+               getMessages()
+            }
+         )
+         .subscribe()
+   }, [])
 
    return (
       <div className="p-2 h-screen">
@@ -139,33 +149,33 @@ export const App = () => {
                : serverError ? <ServerError bg />
                   : user && (
                      <div className='bg-white-1 dark:bg-black-1 rounded shadow-lg w-full container mx-auto h-full relative overflow-hidden flex flex-col'>
-                       <div className="flex justify-between items-center bg-indigo-0 p-2">
-                         <div className="flex items-center gap-3 pl-3">
-                           <span>{user.nick_name}</span>
-                           <Link to="/updateUser" title="Editar perfil"><PencilSimple className="text-xl" /></Link>
-                         </div>
-                         <ThemeToggle position="right" />
-                       </div>
+                        <div className="flex justify-between items-center bg-indigo-0 p-2">
+                           <div className="flex items-center gap-3 pl-3">
+                              <span>{user.nick_name}</span>
+                              <Link to="/updateUser" title="Editar perfil"><PencilSimple className="text-xl" /></Link>
+                           </div>
+                           <ThemeToggle position="right" />
+                        </div>
 
-                       <div className="flex-1 overflow-hidden p-3 overflow-y-auto flex flex-col gap-3">
-                         {
-                           messages &&
-                           messages.map(element => {
-                             return (
-                               <div key={element.id} className={`p-3 rounded-xl ${user.nick_name === element.posted_by ? "bg-indigo-2 text-right self-end" : "bg-white-2 dark:bg-black-2 self-start"}`}>
-                                 <span className="block text-sm pb-2">{element.posted_by}</span>
-                                 {element.content}
-                               </div>
-                             )
-                           })
-                         }
-                       </div>
+                        <div className="flex-1 overflow-hidden p-3 overflow-y-auto flex flex-col gap-3">
+                           {
+                              messages &&
+                              messages.map(element => {
+                                 return (
+                                    <div key={element.id} className={`p-3 rounded-xl ${user.nick_name === element.posted_by ? "bg-indigo-2 text-right self-end" : "bg-white-2 dark:bg-black-2 self-start"}`}>
+                                       <span className="block text-sm pb-2">{element.posted_by}</span>
+                                       {element.content}
+                                    </div>
+                                 )
+                              })
+                           }
+                        </div>
 
-                       {/* <form className="w-full bg-indigo-1 p-2 flex gap-2" onSubmit={handleNewPost}>
-                         <label htmlFor="message" className="hidden">Menssagem</label>
-                         <input type="text" id="message" name="message" placeholder="Digite sua mensagem..." className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
-                         <button>Enviar</button>
-                       </form> */}
+                        <form className="w-full bg-indigo-1 p-2 flex gap-2" onSubmit={handleNewPost}>
+                           <label htmlFor="message" className="hidden">Menssagem</label>
+                           <input type="text" id="message" name="message" placeholder="Digite sua mensagem..." className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
+                           <button>Enviar</button>
+                        </form>
                      </div>
                   )
          }
