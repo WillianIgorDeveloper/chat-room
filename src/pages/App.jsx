95a4a3e3a857eed89bf0d0ba91310ed57d369907
Willian Igor <willianigordeveloper@gmail.com>
2022-11-28 23:14:17 -0300

    try to fixing login

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -16,27 +16,36 @@ export const App = () => {
   const [loading, setLoading] = useState(true)
   const [serverError, setServerError] = useState(false)
 
-  const session = useGetSession("login")
+   const getUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+      console.log(user)
+   }
+  
+   useEffect(() => {
+      getUser()
+   }, [])
 
-  const user = useGetUser(session, setLoading, setServerError)
+  // const session = useGetSession("login")
 
-  const messages = useGetMessages(setServerError)
+  // const user = useGetUser(session, setLoading, setServerError)
 
-  const handleNewPost = async (e) => {
-    e.preventDefault()
-    const newMessage = e.target.message.value
-    e.target.message.value = ""
-    try {
-      const { error } = await supabase
-        .from('message')
-        .insert([
-          { user_id: session.user.id, message: newMessage, user_name: user.userName },
-        ])
-      if (error) throw error
-    } catch (error) {
-      setServerError(true)
-    }
-  }
+  // const messages = useGetMessages(setServerError)
+
+  // const handleNewPost = async (e) => {
+  //   e.preventDefault()
+  //   const newMessage = e.target.message.value
+  //   e.target.message.value = ""
+  //   try {
+  //     const { error } = await supabase
+  //       .from('message')
+  //       .insert([
+  //         { user_id: session.user.id, message: newMessage, user_name: user.userName },
+  //       ])
+  //     if (error) throw error
+  //   } catch (error) {
+  //     setServerError(true)
+  //   }
+  // }
 
   return (
     <div className="p-2 h-screen">
@@ -44,35 +53,36 @@ export const App = () => {
         loading ? <Loading bg />
           : serverError ? <ServerError bg />
             : (
-              <div className='bg-white-1 dark:bg-black-1 rounded shadow-lg w-full container mx-auto h-full relative overflow-hidden flex flex-col'>
-                <div className="flex justify-between items-center bg-indigo-0 p-2">
-                  <div className="flex items-center gap-3 pl-3">
-                    <span>{user.userName}</span>
-                    <Link to="/updateUser" title="Editar perfil"><PencilSimple className="text-xl" /></Link>
-                  </div>
-                  <ThemeToggle position="right" />
-                </div>
+              <h1>Oi</h1>
+              // <div className='bg-white-1 dark:bg-black-1 rounded shadow-lg w-full container mx-auto h-full relative overflow-hidden flex flex-col'>
+              //   <div className="flex justify-between items-center bg-indigo-0 p-2">
+              //     <div className="flex items-center gap-3 pl-3">
+              //       <span>{user.userName}</span>
+              //       <Link to="/updateUser" title="Editar perfil"><PencilSimple className="text-xl" /></Link>
+              //     </div>
+              //     <ThemeToggle position="right" />
+              //   </div>
 
-                <div className="flex-1 overflow-hidden p-3 overflow-y-auto flex flex-col gap-3">
-                  {
-                    messages &&
-                    messages.map(element => {
-                      return (
-                        <div key={element.id} className={`p-3 rounded-xl ${user.userName === element.user_name ? "bg-indigo-2 text-right self-end" : "bg-white-2 dark:bg-black-2 self-start"}`}>
-                          <span className="block text-sm pb-2">{element.user_name}</span>
-                          {element.message}
-                        </div>
-                      )
-                    })
-                  }
-                </div>
+              //   <div className="flex-1 overflow-hidden p-3 overflow-y-auto flex flex-col gap-3">
+              //     {
+              //       messages &&
+              //       messages.map(element => {
+              //         return (
+              //           <div key={element.id} className={`p-3 rounded-xl ${user.userName === element.user_name ? "bg-indigo-2 text-right self-end" : "bg-white-2 dark:bg-black-2 self-start"}`}>
+              //             <span className="block text-sm pb-2">{element.user_name}</span>
+              //             {element.message}
+              //           </div>
+              //         )
+              //       })
+              //     }
+              //   </div>
 
-                <form className="w-full bg-indigo-1 p-2 flex gap-2" onSubmit={handleNewPost}>
-                  <label htmlFor="message" className="hidden">Menssagem</label>
-                  <input type="text" id="message" name="message" placeholder="Digite sua mensagem..." className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
-                  <button>Enviar</button>
-                </form>
-              </div>
+              //   <form className="w-full bg-indigo-1 p-2 flex gap-2" onSubmit={handleNewPost}>
+              //     <label htmlFor="message" className="hidden">Menssagem</label>
+              //     <input type="text" id="message" name="message" placeholder="Digite sua mensagem..." className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
+              //     <button>Enviar</button>
+              //   </form>
+              // </div>
             )
       }
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,72 +13,69 @@ import { useGetSession } from "../hooks/useGetSession"
 
 export const Login = () => {
 
-  const [loading, setLoading] = useState(false)
-  const [serverError, setServerError] = useState(false)
-  const [mailSended, setMailSended] = useState(false)
+   const [loading, setLoading] = useState(false)
+   const [serverError, setServerError] = useState(false)
+   const [mailSended, setMailSended] = useState(false)
 
-  const session = useGetSession("app")
+   const session = useGetSession("app")
 
-  async function signInWithEmail(e) {
-
-    e.preventDefault()
-
-    const email = e.target.email.value
-
-    try {
+   async function signInWithEmail(e) {
+      e.preventDefault()
       setLoading(true)
-      const { error } = await supabase.auth.signInWithOtp({ email })
-      if (error) throw error
-      setMailSended(true)
-    }
-    catch (error) {
-      setServerError(true)
-    }
-    finally {
-      setLoading(false)
-    }
-  }
+      const newEmail = e.target.email.value
+      try {
+         const { error } = await supabase.auth.signInWithOtp({ email: newEmail })
+         if (error) throw error
+         setMailSended(true)
+      }
+      catch (error) {
+         setServerError(true)
+      }
+      finally {
+         setLoading(false)
+      }
+   }
 
-  async function signInWithDiscord() {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'discord',
-    })
-    error && setServerError(true)
-  }
+   async function signInWithDiscord() {
+      const { error } = await supabase.auth.signInWithOAuth({
+         provider: 'discord',
+      })
+      error && setServerError(true)
+   }
 
-  async function signInWithGitHub() {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'github',
-    })
-    error && setServerError(true)
-  }
+   async function signInWithGitHub() {
+      const { error } = await supabase.auth.signInWithOAuth({
+         provider: 'github',
+      })
+      error && setServerError(true)
+   }
 
-  return (
-    <div className='h-screen w-full flex flex-col items-center justify-center p-4'>
-      <ThemeToggle fixed={true} position="right" />
-      <Logo />
-      <div className='bg-white-1 dark:bg-black-1 p-5 mt-4 rounded shadow-lg w-full container mx-auto'>
-        {
-          loading ? <Loading />
-            : mailSended ? <MailSended />
-              : serverError ? <ServerError />
-                : (
-                  <div className='max-w-lg m-auto'>
-                    <span className='block text-center pb-5 text-xl font-semibold'>Login</span>
-                    <div className='flex flex-col gap-5'>
-                      <Button onClick={signInWithDiscord}><DiscordLogo />Entrar com Discord</Button>
-                      <Button onClick={signInWithGitHub}><GithubLogo />Entrar com Github</Button>
-                    </div>
-                    <form className='flex flex-col items-center gap-2 pt-5' onSubmit={signInWithEmail}>
-                      <legend className='hidden'>Login</legend>
-                      <label htmlFor="email" className='flex items-center justify-center gap-3'>Entrar com link mágico <MagicWand /></label>
-                      <input type="email" name="email" id="email" placeholder='Digite seu e-mail...' className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
-                      <Button type='submit'>Enviar</Button>
-                    </form>
-                  </div>
-                )
-        }
+   return (
+      <div className='h-screen w-full flex flex-col items-center justify-center p-4'>
+         <ThemeToggle fixed={true} position="right" />
+         <Logo />
+         <div className='bg-white-1 dark:bg-black-1 p-5 mt-4 rounded shadow-lg w-full container mx-auto'>
+            {
+               loading ? <Loading />
+                  : mailSended ? <MailSended />
+                     : serverError ? <ServerError />
+                        : (
+                           <div className='max-w-lg m-auto'>
+                              <span className='block text-center pb-5 text-xl font-semibold'>Login</span>
+                              <div className='flex flex-col gap-5'>
+                                 <Button onClick={signInWithDiscord}><DiscordLogo />Entrar com Discord</Button>
+                                 <Button onClick={signInWithGitHub}><GithubLogo />Entrar com Github</Button>
+                              </div>
+                              <form className='flex flex-col items-center gap-2 pt-5' onSubmit={signInWithEmail}>
+                                 <legend className='hidden'>Login</legend>
+                                 <label htmlFor="email" className='flex items-center justify-center gap-3'>Entrar com link mágico <MagicWand /></label>
+                                 <input type="email" name="email" id="email" placeholder='Digite seu e-mail...' className='bg-white-2 text-black-1 py-2 px-4 w-full rounded focus:outline-indigo-2' />
+                                 <Button type='submit'>Enviar</Button>
+                              </form>
+                           </div>
+                        )
+            }
+         </div>
       </div>
-    </div>
-  )
+   )
 }
